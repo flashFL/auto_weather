8de9fad9bc9435d895da66ba176817e6d5177f5b
unknown <976668382@qq.com>
2020-02-15 20:16:32 +0800

    2

--- weather.py
+++ weather.py
@@ -7,9 +7,23 @@ from selenium import webdriver
 def get_weather(url):
     req = requests.get(url)
     req.encoding = 'utf-8'
-    aim = re.findall('<input type="hidden" id="hidden_title" value="(.*?)月(.*?)日08时 (.*?)  (.*?)  (.*?)" />', req.text, re.S)
-    print('%s月%s日 %s %s' % aim[0][:4])
-    print('今日气温：' + aim[0][4])
+    aim = re.findall('<input type="hidden" id="hidden_title" value="(.*?)月(.*?)日20时 (.*?)  (.*?)  (.*?)" />', req.text, re.S)
+
+    fout = open('result.html', 'w')
+    fout.write('<html>') #设置输出的HTML文件的格式
+    fout.write('<body>')
+    fout.write('<table>')
+    fout.write("<tr>")
+    fout.write("<td>%s月%s日 %s %s</td>" % aim[0][:4])  # 将爬取的信息写入到html文件中
+    fout.write("</tr>")
+    fout.write("<tr>")
+    fout.write("<td>今日气温：%s</td>" % aim[0][4])
+    fout.write("</tr>")
+    # print('%s月%s日 %s %s' % aim[0][:4])
+    # print('今日气温：' + aim[0][4])
+    fout.write("</table>")
+    fout.write("</body>")
+    fout.write("</html>")
 
 
 def real_time_weather(url):
